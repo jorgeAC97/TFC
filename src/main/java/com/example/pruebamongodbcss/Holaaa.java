52907b5f37aa3897d6cbfab8e61ceeaa326e2997
Cristopher <cristofer.torres.ct@gmail.com>
2025-04-17 18:25:51 +0200

    Pongo ADIOSSS

--- src/main/java/com/example/pruebamongodbcss/Holaaa.java
+++ src/main/java/com/example/pruebamongodbcss/Holaaa.java
@@ -3,5 +3,9 @@ package com.example.pruebamongodbcss;
 public class Holaaa {
     public static void main(String[] args) {
         System.out.println("Holaaa");
+
+
+        System.out.println("Adiossss");
+
     }
 }
